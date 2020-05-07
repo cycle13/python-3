688aa1b05bccf81dd4a6defb21e5845c46e74e9d
Juan <gustfrontar@gmail.com>
2020-05-07 19:32:05 -0300

    Scale to radar for python V1.0

--- python_scripts/scale_to_radar/src/python/pawr_read.py
+++ python_scripts/scale_to_radar/src/python/pawr_read.py
@@ -3,9 +3,15 @@ import numpy.ma as ma
 from .fortranio import *
 import datetime as dt
 import time
-import pyart
+import struct
 
-def pawr_read(filename, endian=''):
+
+
+
+Re = 6370.e3 # Earth radius in (m)
+
+
+def pawr_read(filename, endian='',minref=0.0):
     dtype_real = endian + 'f4'
     dtype_int = endian + 'i4'
 
@@ -23,89 +29,267 @@ def pawr_read(filename, endian=''):
 
     buf = np.zeros(8, dtype=dtype_real)
     fort_seq_read(f, buf)
-    radar_lon  = buf[0]
-    radar_lat  = buf[1]
-    radar_alt  = buf[2]
-    beam_wid_h = buf[3]
-    beam_wid_v = buf[4]
-    beam_wid_r = buf[5]
-    rlambda    = buf[6]
-    undef      = buf[7]
+    data['radar_lon'] = buf[0]
+    data['radar_lat'] = buf[1]
+    data['radar_alt'] = buf[2]
+    data['beam_wid_h'] = buf[3]
+    data['beam_wid_v'] = buf[4]
+    data['beam_wid_r'] = buf[5]
+    data['lambda'] = buf[6]
+    data['undef'] = buf[7]
+    data['minref']= minref
 
     buf = np.zeros(4, dtype=dtype_int)
     fort_seq_read(f, buf)
-    na = buf[0]
-    nr = buf[1]
-    ne = buf[2]
-    nvar = buf[3]
+    data['na'] = buf[0]
+    data['nr'] = buf[1]
+    data['ne'] = buf[2]
+    data['nvar'] = buf[3]
 
-    radar = pyart.testing.make_empty_ppi_radar( nr , na , ne )
-    radar.latitude['data'] = np.array([ radar_lat ])
-    radar.longitude['data'] = np.array([ radar_lon ])
-    radar.altitude['data'] = np.array([ radar_alt ])
-    radar.sweep_number['data'] = np.array([ ne ])
-    
-    radar.ngates=nr
-    radar.nrays=na*ne
+    data['azim'] = np.zeros(data['na'], dtype=dtype_real)
+    fort_seq_read(f, data['azim'])
 
-    azim = np.zeros(na, dtype=dtype_real)
-    fort_seq_read(f, azim)
+    data['radi'] = np.zeros(data['nr'], dtype=dtype_real)
+    fort_seq_read(f, data['radi'])
 
-    radi = np.zeros(nr, dtype=dtype_real)
-    fort_seq_read(f, radi)
-    radar.range['data'] = radi
-
-    elev = np.zeros(ne, dtype=dtype_real)
-    fort_seq_read(f, elev)
-    radar.fixed_angle['data']=elev
+    data['elev'] = np.zeros(data['ne'], dtype=dtype_real)
+    fort_seq_read(f, data['elev'])
 
     buf = np.zeros(1, dtype=dtype_real)
     fort_seq_read(f, buf)
-    attn_fac = buf[0]
+    data['attn_fac'] = buf[0]
 
-    radar.azimuth['data']=np.zeros( na * ne )
-    radar.elevation['data']=np.zeros( na * ne )
-    radar.time['data']=np.zeros( na * ne )
+    buf = np.zeros((data['ne'], data['nr'], data['na']), dtype=dtype_real)
+    for ie in range(data['ne']):
+        fort_seq_read(f, buf[ie])
+    data['ref'] = ma.masked_values(buf, data['undef'])
 
+    for ie in range(data['ne']):
+        fort_seq_read(f, buf[ie])
+    data['rv'] = ma.masked_values(buf, data['undef'])
 
-    buf = np.zeros((ne, nr, na), dtype=dtype_real)
-    for ie in range(ne):
-        fort_seq_read(f, buf[ie,:,:])
-    ref = np.copy( buf )
-    for ie in range(ne):
-        fort_seq_read(f, buf[ie,:,:])
-    rv = np.copy( buf )
+    for ie in range(data['ne']):
+        fort_seq_read(f, buf[ie])
+    data['qc'] = ma.masked_values(buf, data['undef'])
+
+    for ie in range(data['ne']):
+        fort_seq_read(f, buf[ie])
+    data['attn'] = ma.masked_values(buf, data['undef'])
 
     f.close()
 
-    #Save variables into radar object
-    ref_dict = pyart.config.get_metadata('reflectivity')
-    ref_dict['data'] = np.zeros( (na * ne , nr) )
-    rv_dict = pyart.config.get_metadata('velocity')
-    rv_dict['data'] = np.zeros( (na * ne , nr) )
+    mask_ = np.logical_and( data['qc'] == 0.0 , data['ref'] < minref )
+    data['ref'][mask_]=minref
 
-    iray=0
-    position=0
-    ref_cf=np.zeros( (na * ne , nr) )
-    rv_cf =np.zeros( (na * ne , nr) )
-    for jj in range( ne ) :
-        for ii in range( na ) :
-            ref_cf[iray,:]=ref[jj,:,ii]
-            rv_cf[iray,:] =rv[jj,:,ii]
-            radar.azimuth['data'][iray] = azim[ii]
-            radar.elevation['data'][iray] = elev[jj]
-            iray=iray+1
-
-    ref_dict['data'] = ma.masked_values(ref_cf, undef)
-    rv_dict['data']  = ma.masked_values(rv_cf, undef)
-
-    radar.fields = {'rv': rv_dict , 'ref':ref_dict}
-
-    radar.ray_angle_res=dict()
-    radar.ray_angle_res['data'] = 360.0 / na.astype(float)
 
 
     print("Radar data '{:s}' was read in {:.3f} seconds".format(filename, time.time() - t0))
-    return radar
+
+    t0 = time.time()
+
+    data = antenna_to_cartesian( data )
+
+    data = cartesian_to_geographic_aeqd( data, R=6370997.)
+
+    print("Radar data '{:s}' was georeferenced in {:.3f} seconds".format(filename, time.time() - t0))
+
+    return data
+
+
+def dist_ll(lon1, lat1, lon2, lat2):
+    return Re * np.arccos(np.sin(np.deg2rad(lat1)) * np.sin(np.deg2rad(lat2)) + 
+                          np.cos(np.deg2rad(lat1)) * np.cos(np.deg2rad(lat2)) * np.cos(np.deg2rad(lon2 - lon1)))
+
+
+def az_ll(lon1, lat1, lon2, lat2):
+    return np.rad2deg(np.arctan2(np.sin(np.deg2rad(lon2 - lon1)) * np.cos(np.deg2rad(lat2)),
+                                 np.cos(np.deg2rad(lat1)) * np.sin(np.deg2rad(lat2)) - np.sin(np.deg2rad(lat1)) * np.cos(np.deg2rad(lat2)) * np.cos(np.deg2rad(lon2 - lon1))))
+
+
+def ll_arc_distance(lon0, lat0, arc_dist, az):
+    if arc_dist == 0.:
+        lon = lon0
+        lat = lat0
+    else:
+        cdist = np.cos(arc_dist / Re)
+        sdist = np.sin(arc_dist / Re)
+        sinll1 = np.sin(np.deg2rad(lat0))
+        cosll1 = np.cos(np.deg2rad(lat0))
+        lat = np.rad2deg(np.arcsin(sinll1 * cdist + cosll1 * sdist * np.cos(np.deg2rad(az))))
+        lon = lon0 + np.rad2deg(np.arctan2(sdist * np.sin(np.deg2rad(az)), 
+                                           cosll1 * cdist - sinll1 * sdist * np.cos(np.deg2rad(az))))
+    return lon, lat
+
+
+def radar_georeference(data, lon=None, lat=None, radi_h=None):
+    Ns = 1.21
+    ke = 4. / 3.
+
+    data['r_earth'] = Re
+
+    data['symhgt'] = np.zeros((data['ne'], data['nr']), dtype='f4')
+
+    for ie in range(data['ne']):
+        for ir in range(data['nr']):
+            # Two possibilities the second probably more accurate than the
+            # first one, but both assume a standard constant value for the
+            # refractivity index.
+            #data['symhgt'][ie,ir] = data['radar_alt'] \
+            #                   + data['radi'][ir] * np.sin(np.deg2rad(data['elev'][ie])) \
+            #                   + (data['radi'][ir] ** 2) / (2. * Ns * Re)
+            data['symhgt'][ie,ir] = data['radar_alt'] \
+                               + np.sqrt(data['radi'][ir] ** 2 + (ke * Re) ** 2 +
+                                         2. * data['radi'][ir] * ke * Re * np.sin(np.deg2rad(data['elev'][ie]))) \
+                               - ke * Re
+
+    data['radi_h'] = np.zeros((data['ne'], data['nr']), dtype='f4')
+    data['lon'] = np.zeros((data['ne'], data['nr'], data['na']), dtype='f4')
+    data['lat'] = np.zeros((data['ne'], data['nr'], data['na']), dtype='f4')
+    data['hgt'] = np.zeros((data['ne'], data['nr'], data['na']), dtype='f4')
+
+    #if (lon is None) or (lat is None) or (radi_h is None):
+    for ie in range(data['ne']):
+
+         print('ie =', ie)
+
+         # The curvature of the radar beam is not taken into account.
+         data['radi_h'][ie,:] = ke * Re * np.arcsin(data['radi'] * np.cos(np.deg2rad(data['elev'][ie])) / (ke * Re)) # Radar horizontal range
+
+         for ir in range(data['nr']):
+             for ia in range(data['na']):
+                 data['lon'][ie,ir,ia], data['lat'][ie,ir,ia] = \
+                     ll_arc_distance(data['radar_lon'], data['radar_lat'], data['radi_h'][ie,ir], data['azim'][ia])
+
+#        np.save('radi_h.npy', data['radi_h'])
+#        np.save('lon.npy', data['lon'])
+#        np.save('lat.npy', data['lat'])
+
+    #else:
+    #    data['radi_h'] = np.load(radi_h)
+    #    data['lon'] = np.load(lon)
+    #    data['lat'] = np.load(lat)
+
+
+    for ia in range(data['na']):
+        data['hgt'][:,:,ia] = data['symhgt']
+
+    return True
+
+
+def antenna_to_cartesian(data):
+    """
+    Return Cartesian coordinates from antenna coordinates.
+    Parameters
+    ----------
+    ranges : array
+        Distances to the center of the radar gates (bins) in kilometers.
+    azimuths : array
+        Azimuth angle of the radar in degrees.
+    elevations : array
+        Elevation angle of the radar in degrees.
+    Returns
+    -------
+    x, y, z : array
+        Cartesian coordinates in meters from the radar.
+    Notes
+    -----
+    The calculation for Cartesian coordinate is adapted from equations
+    2.28(b) and 2.28(c) of Doviak and Zrnic [1]_ assuming a
+    standard atmosphere (4/3 Earth's radius model).
+    .. math::
+        z = \\sqrt{r^2+R^2+2*r*R*sin(\\theta_e)} - R
+        s = R * arcsin(\\frac{r*cos(\\theta_e)}{R+z})
+        x = s * sin(\\theta_a)
+        y = s * cos(\\theta_a)
+    Where r is the distance from the radar to the center of the gate,
+    :math:`\\theta_a` is the azimuth angle, :math:`\\theta_e` is the
+    elevation angle, s is the arc length, and R is the effective radius
+    of the earth, taken to be 4/3 the mean radius of earth (6371 km).
+    References
+    ----------
+    .. [1] Doviak and Zrnic, Doppler Radar and Weather Observations, Second
+        Edition, 1993, p. 21.
+    """
+    [ elevations , r , azimuths ] = np.meshgrid( data['elev'] , data['radi'] , data['azim'] , indexing='ij' )
+
+    theta_e = elevations * np.pi / 180.0    # elevation angle in radians.
+    theta_a = azimuths * np.pi / 180.0      # azimuth angle in radians.
+    R = 6371.0 * 1000.0 * 4.0 / 3.0         # effective radius of earth in meters.
+
+    data['z'] = (r ** 2 + R ** 2 + 2.0 * r * R * np.sin(theta_e)) ** 0.5 - R
+    s = R * np.arcsin(r * np.cos(theta_e) / (R + data['z']))  # arc length in m.
+    data['x'] = s * np.sin(theta_a)
+    data['y'] = s * np.cos(theta_a)
+
+    return data
+
+
+def cartesian_to_geographic_aeqd(data, R=6370997.) :
+    """
+    Azimuthal equidistant Cartesian to geographic coordinate transform.
+    Transform a set of Cartesian/Cartographic coordinates (x, y) to
+    geographic coordinate system (lat, lon) using a azimuthal equidistant
+    map projection [1]_.
+    .. math::
+        lat = \\arcsin(\\cos(c) * \\sin(lat_0) +
+                       (y * \\sin(c) * \\cos(lat_0) / \\rho))
+        lon = lon_0 + \\arctan2(
+            x * \\sin(c),
+            \\rho * \\cos(lat_0) * \\cos(c) - y * \\sin(lat_0) * \\sin(c))
+        \\rho = \\sqrt(x^2 + y^2)
+        c = \\rho / R
+    Where x, y are the Cartesian position from the center of projection;
+    lat, lon the corresponding latitude and longitude; lat_0, lon_0 are the
+    latitude and longitude of the center of the projection; R is the radius of
+    the earth (defaults to ~6371 km). lon is adjusted to be between -180 and
+    180.
+    Parameters
+    ----------
+    x, y : array-like
+        Cartesian coordinates in the same units as R, typically meters.
+    lon_0, lat_0 : float
+        Longitude and latitude, in degrees, of the center of the projection.
+    R : float, optional
+        Earth radius in the same units as x and y. The default value is in
+        units of meters.
+    Returns
+    -------
+    lon, lat : array
+        Longitude and latitude of Cartesian coordinates in degrees.
+    References
+    ----------
+    .. [1] Snyder, J. P. Map Projections--A Working Manual. U. S. Geological
+        Survey Professional Paper 1395, 1987, pp. 191-202.
+    """
+    x = np.atleast_1d(np.asarray(data['x']))
+    y = np.atleast_1d(np.asarray(data['y']))
+
+    lat_0_rad = np.deg2rad(data['radar_lat'])
+    lon_0_rad = np.deg2rad(data['radar_lon'])
+
+    rho = np.sqrt(x*x + y*y)
+    c = rho / R
+
+    #with warnings.catch_warnings():
+    # division by zero may occur here but is properly addressed below so
+    # the warnings can be ignored
+    #warnings.simplefilter("ignore", RuntimeWarning)
+    lat_rad = np.arcsin(np.cos(c) * np.sin(lat_0_rad) +
+                            y * np.sin(c) * np.cos(lat_0_rad) / rho)
+    data['lat_gate'] = np.rad2deg(lat_rad)
+    # fix cases where the distance from the center of the projection is zero
+    data['lat_gate'][rho == 0] = data['radar_lat']
+
+    x1 = x * np.sin(c)
+    x2 = rho*np.cos(lat_0_rad)*np.cos(c) - y*np.sin(lat_0_rad)*np.sin(c)
+    lon_rad = lon_0_rad + np.arctan2(x1, x2)
+    data['lon_gate'] = np.rad2deg(lon_rad)
+    # Longitudes should be from -180 to 180 degrees
+    data['lon_gate'][data['lon_gate'] > 180] -= 360.
+    data['lon_gate'][data['lon_gate'] < -180] += 360.
+
+    return data
+
+
 
 
--- python_scripts/scale_to_radar/test_scale_to_radar.py
+++ python_scripts/scale_to_radar/test_scale_to_radar.py
@@ -4,9 +4,21 @@ import calc
 #import calc_tmp
 import matplotlib.pyplot as plt
 import pyart
+import datetime as dt
+import numpy as np
+
 
 exp_path='/home/juan/tmp_scale_to_radar/output_data/LE_D1_1km_30sec_OFP_V2/'
 rad_path='/home/juan/tmp_scale_to_radar/radar_data/'
+
+init_freq=600   #Forecast initialization frequency in seconds.
+out_freq =30    #Forecast output frequency in seconds.
+for_len  =1800  #Forecast length in seconds.
+
+minref=0.0      #Ref values below this threshold will be assumed equal to the threshold.
+
+itime = dt.datetime(2013,7,13,5,10,0)  #Initial time.
+etime = dt.datetime(2013,7,13,6,0,0)   #End time.
 
 
 
@@ -23,6 +35,22 @@ proj = {
 'LC_lat2': 37.5
 }
 
+
+#Time loop to read forecast and interpolate them to nearest radar data.
+ctime = itime
+while ( ctime <= etime ):
+
+    fi_time = ctime
+    fe_time = ctime + dt.timedelta( seconds = for_len )
+    cftime = fi_time
+
+
+
+
+    ctime = ctime + dt.timedelta( seconds = init_freq )
+
+#Time loop to read forecast and interpolate them to nearest radar data.
+
 #Generate file object.
 sio = io.ScaleIO(data_path, verbose=1)
 sioh = io.ScaleIO(topo_path , verbose=1)
@@ -31,7 +59,9 @@ topo = sioh.readvar('TOPO',bufsize=2)
 #Read radar
 radar = pawr_read.pawr_read( radar_file )
 #Read and interpolate model data
-[model_ref , model_vr ]=calc.radar_int( sio , proj , topo , radar , t=10 ) 
+[model_ref , model_rv ]=calc.radar_int( sio , proj , topo , radar , t=10 ) 
+
+model_ref[ model_ref < minref ] = minref
 
 #Add interpolated data to radar object.
 undef = radar.fields['ref']['data'].fill_value
@@ -39,14 +69,11 @@ model_ref[ radar.fields['ref']['data'].mask  ] = undef
 model_rv [ radar.fields['rv']['data'].mask   ] = undef
 
 ref_dict = pyart.config.get_metadata('reflectivity')
-ref_dict['data'] = ma.masked_values(model_ref, undef)
+ref_dict['data'] = np.ma.masked_values(model_ref, undef)
 rv_dict = pyart.config.get_metadata('velocity')
-rv_dict['data'] = ma.masked_values(model_rv, undef)
+rv_dict['data'] = np.ma.masked_values(model_rv, undef)
 radar.fields['model_ref']=ref_dict
 radar.fields['model_rv'] =rv_dict
-
-#bmap = calc.set_bmap(sio, proj, resolution=None, rtol=1.e-6)
-#[dbz,rv,max_dbz]=calc.calc_ref_rv(sio, bmap , radar , topo ,  min_dbz=-20., rho=None, qr=None, qs=None, qg=None, t=10, dryrun=False)
 
 
 plt.pcolor( radar.fields['model_ref']['data'][3000:3300,:] )
@@ -55,6 +82,3 @@ plt.show()
 plt.pcolor( radar.fields['model_rv']['data'][3000:3300,:] )
 plt.show()
 
-#plt.pcolor( rv[10,:,:] )
-#plt.colorbar()
-#plt.show()
