55ef23845e977440510b4c338562e27cb671e478
Juan <gustfrontar@gmail.com>
2020-05-09 15:36:14 -0300

    Several bug corrections in scale_to_radar

--- python_scripts/scale_to_radar/setup.sh
+++ python_scripts/scale_to_radar/setup.sh
@@ -4,3 +4,5 @@
 cd ./src/python/
 f2py  -c -lgomp --f90flags="-fopenmp -lgomp -O3" -m calc_for ../fortran/calc_for.f90
 
+f2py  -c -lgomp --f90flags="-fopenmp -lgomp -O3" -m radar_tools ../fortran/radar_tools.f90
+
